68ca8d696f7974e716ba58d05c46b554f063330d
Junrui Liu <junruiliu@vassar.edu>
2019-11-23 16:42:08 -0500

    New interface to make and parse packets

---
+++ src/packet.c
@@ -0,0 +1,70 @@
+//
+//  packet.c
+//  peer
+//
+//  Created by work on 11/23/19.
+//  Copyright © 2019 team1. All rights reserved.
+//
+
+#include <stdint.h>
+#include <arpa/inet.h>
+#include "packet.h"
+
+// (offset, size) pairs
+int packet_field_info [8][2] = {
+    {0, 2},  // 0. Magic Number          [2 B, uint16_t]
+    {2, 1},  // 1. Version Number        [1 B, uint8_t ]
+    {3, 1},  // 2. Packet Type           [1 B, uint8_t ]
+    {4, 2},  // 3. Header Length         [2 B, uint16_t]
+    {6, 2},  // 4. Total Packet Length   [2 B, uint16_t]
+    {8, 4},  // 5. Sequence Number       [4 B, uint32_t]
+    {12, 4}, // 6. Acknowledgment Number [4 B, uint32_t]
+}
+
+
+int make_packet(char* buf,
+                uint8_t packet_type,
+                uint16_t  header_len,
+                uint16_t  packet_len,
+                uint32_t seq_no,
+                uint32_t ack_no,
+                char* payload,
+                size_t payload_len)
+{
+    MAKE_FIELD(buf, P_MAGIC, htons(MAGIC_NUMBER));
+    MAKE_FIELD(buf, P_VERSN, VERSION);
+    MAKE_FIELD(buf, P_PTYPE, packet_type);
+    MAKE_FIELD(buf, P_HDLEN, htons(header_len));
+    MAKE_FIELD(buf, P_PKLEN, htons(packet_len));
+    MAKE_FIELD(buf, P_SEQNO, htonl(seq_no));
+    MAKE_FIELD(buf, P_ACKNO, htonl(ack_no));
+    memcpy(buf + HEAD_LEN_NORMAL, payload, payload_len);
+}
+
+
+
+int parse_packet(char* buf,
+                 uint16_t* magic_no,
+                 uint8_t*  version,
+                 uint8_t* packet_type,
+                 uint16_t* header_len,
+                 uint16_t* packet_len,
+                 uint32_t* seq_no,
+                 uint32_t* ack_no,
+                 char** payload)
+{
+    EXTRACT_FIELD(buf, P_MAGIC, magic_no);
+    EXTRACT_FIELD(buf, P_VERSN, version);
+    EXTRACT_FIELD(buf, P_PTYPE, packet_type);
+    EXTRACT_FIELD(buf, P_HDLEN, header_len);
+    EXTRACT_FIELD(buf, P_PKLEN, packet_len);
+    EXTRACT_FIELD(buf, P_SEQNO, seq_no);
+    EXTRACT_FIELD(buf, P_ACKNO, ack_no);
+    *payload = buf + HEAD_LEN_NORMAL;
+    
+    *magic_no = ntohs(*magic_no);
+    *header_len = ntohs(*header_len);
+    *packet_len = ntohs(*packet_len);
+    *seq_no = ntohl(*seq_no);
+    *ack_no = ntohl(*ack_no);
+}
---
+++ src/packet.h
@@ -0,0 +1,73 @@
+//
+//  packet.h
+//  peer
+//
+//  Created by work on 11/23/19.
+//  Copyright © 2019 team1. All rights reserved.
+//
+
+#ifndef packet_h
+#define packet_h
+
+// Index into |packet_field_info| array
+#define P_MAGIC 0
+#define P_VERSN 1
+#define P_PTYPE 2
+#define P_HDLEN 3
+#define P_PKLEN 4
+#define P_SEQNO 5
+#define P_ACKNO 6
+
+
+#define PTYPE_WHOHAS 0
+#define PTYPE_IHAVE  1
+#define PTYPE_GET    2
+#define PTYPE_DATA   3
+#define PTYPE_ACK    4
+#define PTYPE_DENIED 5
+
+
+#define MAGIC_NUMBER 3752
+#define VERSION 1
+#define HEAD_LEN_NORMAL 16
+#define FILED_N_A 0
+
+
+int make_packet(char* buf,
+                uint8_t packet_type,
+                uint16_t  header_len,
+                uint16_t  packet_len,
+                uint32_t seq_no,
+                uint32_t ack_no,
+                char* payload);
+
+
+
+int parse_packet(char* buf,
+                 uint16_t* magic_no,
+                 uint8_t*  version,
+                 uint8_t* packet_type,
+                 uint16_t* header_len,
+                 uint16_t* packet_len,
+                 uint32_t* seq_no,
+                 uint32_t* ack_no,
+                 char** payload);
+
+
+
+#define MAKE_FIELD(buf, field_name, field_val) \
+    do { \
+        memcpy(buf[packet_field_info[field_name][1]], \
+               field_val, \
+               packet_field_info[field_name][0]);\
+    } while (0)
+
+#define EXTRACT_FIELD(buf, field_name, field_ptr) \
+    do { \
+        memcpy(field_ptr, \
+               buf[packet_field_info[field_name][1]], \
+               packet_field_info[field_name][0]);\
+    } while (0)
+
+
+#endif /* packet_h */
